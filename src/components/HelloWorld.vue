cc40ee408c1e8498c25497aee294dfdf66e7e124
prashantupreti <38235022+prashantupreti@users.noreply.github.com>
2021-06-28 02:35:35 -0500

    Updated
    updated

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,17 +2,13 @@
   <div>    
     <div class="mt-6" v-for="(post,index) in posts" v-bind:key="index">
         <div class="max-w-4xl px-10 py-6 mx-auto bg-white rounded-lg shadow-md">
-            <div class="flex items-center justify-between"><span class="font-light text-gray-600">{{getPostDate(post.date)}}</span><a href="#"
-                    class="px-2 py-1 font-bold text-gray-100 bg-gray-600 rounded hover:bg-gray-500">Testing</a>
+            <div class="flex items-center justify-between"><span class="font-light text-gray-600">{{getPostDate(post.date)}}</span><span v-for="(cat,index) in post.categories" v-bind:key="index"><a href="#"
+                    class="px-2 py-1 font-bold text-gray-100 bg-gray-600 rounded hover:bg-gray-500">{{cat}}</a></span>
             </div>
             <div class="mt-2"><a href="#" class="text-2xl font-bold text-gray-700 hover:underline">{{post.title.rendered}}</a>
                 <p class="mt-2 text-gray-600" v-html="post.excerpt.rendered"></p>
             </div>
-            <div class="flex items-center justify-between mt-4"><a href="#"
-                    class="text-blue-500 hover:underline">Read more</a>
-                <div><a href="#" class="flex items-center">
-                        <h1 class="font-bold text-gray-700 hover:underline">{{post.name}}</h1>
-                    </a></div>
+            <div class="flex items-center justify-between mt-4"><router-link :to="'/blog/'+getYear(post.date)+'/'+getMonth(post.date)+'/'+getDay(post.date)+'/'+post.slug" class="text-blue-500 hover:underline">Read more</router-link>
             </div>
         </div>
     </div>
@@ -35,6 +31,15 @@ export default {
       posts:[]
     }
   },
+  beforeRouteEnter (to, from, next) {
+    next(vm => vm.getRecentMessages())
+  },
+  // when route changes and this component is already rendered,
+  // the logic will be slightly different.
+  beforeRouteUpdate (to, from, next) {
+      this.getRecentMessages()
+      next()
+  },
   methods:{
     getRecentMessages(){
       axios.get(this.postsUrl, {params:this.queryOptions})
@@ -47,7 +52,18 @@ export default {
     },
     getPostDate(date){
       return moment(date).format("lll");
+    },
+    getYear(date){
+      return moment(date).format("Y");
+    },
+    getMonth(date){
+      return moment(date).format("M");
+    },
+    getDay(date){
+      return moment(date).format("D");
     }
+
+
   },
   mounted(){
     this.getRecentMessages();
---
+++ src/views/Post.vue
@@ -0,0 +1,26 @@
+<template>
+    <article class="py-12 px-4">
+        <h1 class="text-4xl text-center mb-4 font-semibold font-heading font-semibold">The Official Dunder Mifflin Scranton Diary</h1>
+        <p class="text-center">
+            <span>October 22, by</span>
+            <a class="ml-1 text-indigo-600 hover:underline" href="#">Michael Scott</a>
+        </p>
+        
+        <div class="max-w-3xl mx-auto">
+            <p class="mb-4">We offer a dependable and quick supply of copy paper suited to all kinds of printers. Each of our various products is designed to provide the finest performance and meet international standards.</p>
+            <p class="mb-4">We’re dedicated to ensure the highest level of customer satisfaction based on long-term professional relationships. By creating the positive working environment we’re enabling our employees to significantly improve not only their productivity, but what’s more important – job satisfaction.</p>
+            <ul class="mb-4 list-inside list-disc">
+            <li>High durability</li>
+            <li>Value-based price</li>
+            <li>Perfect performance on copy machines</li>
+            <li>Long lasting whiteness</li>
+            </ul>
+            <p class="mb-10">We deliver our services with passion and dedication unmatched by other so called “big players”. We create a friendly environment for our workers and that’s what makes their dedication soar to the maximum. You are getting not only the best possible product, but also our love for paper (completely free of charge).</p>
+            <blockquote class="text-center mb-10">
+            <p class="text-lg font-semibold mb-2">"I would say I kind of have an unfair advantage, because I watch reality dating shows like a hawk, and I learn. I absorb information from the strategies of the winners and the losers. Actually, I probably learn more from the losers."</p>
+            <footer class="text-gray-400">Michael Scott</footer>
+            </blockquote>
+            <p>Because the real business is done on paper.</p>
+        </div>
+    </article>    
+</template>
